94dabb9933f71395efe0af0b3242f7fbbaaba795
lucia <luucia.lopezc@gmail.com>
2024-06-05 23:54:00 +0200

    Implementación de las entities Usuario, rol, mascota y especie

---
+++ domain/src/main/java/com/clinica/clinicaVeterinaria/domain/entities/Especie.java
@@ -0,0 +1,36 @@
+package com.clinica.clinicaVeterinaria.domain.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "especie")
+public class Especie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idEspecie;
+    private String nombre;
+
+    public Especie() {
+    }
+
+    public Especie(int idEspecie, String descripcion) {
+        this.idEspecie = idEspecie;
+        this.nombre = descripcion;
+    }
+
+    public int getIdEspecie() {
+        return idEspecie;
+    }
+
+    public void setIdEspecie(int idEspecie) {
+        this.idEspecie = idEspecie;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
--- domain/src/main/java/com/clinica/clinicaVeterinaria/domain/entities/Mascota.java
+++ domain/src/main/java/com/clinica/clinicaVeterinaria/domain/entities/Mascota.java
@@ -1,7 +1,150 @@
 package com.clinica.clinicaVeterinaria.domain.entities;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "mascota")
 public class Mascota {
 
-    //Hola Mundo
-    //Adios Mundo 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idMascota;
+    private String nombre;
+    private int edad;
+    private float peso;
+    private boolean genero;
+    private String complexion;
+    private String urlImagen;
+    private Date fechaNacimiento;
+    private Date fechaAlta;
+
+    @ManyToOne(
+            optional = true,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            }
+    )
+    @JoinColumn(
+            name = "id_usuario"
+    )
+    private Usuario usuario;
+
+    @ManyToOne(
+            optional = true,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            }
+    )
+    @JoinColumn(
+            name = "id_especie"
+    )
+    private Especie especie;
+
+    public Mascota() {
+    }
+
+    public Mascota(int idMascota, String nombre, int edad, float peso, boolean genero, String complexion, String urlImagen, Date fechaNacimiento, Date fechaAlta, Usuario usuario, Especie especie) {
+        this.idMascota = idMascota;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.genero = genero;
+        this.complexion = complexion;
+        this.urlImagen = urlImagen;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaAlta = fechaAlta;
+        this.usuario = usuario;
+        this.especie = especie;
+    }
+
+    public int getIdMascota() {
+        return idMascota;
+    }
+
+    public void setIdMascota(int idMascota) {
+        this.idMascota = idMascota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public boolean isGenero() {
+        return genero;
+    }
+
+    public void setGenero(boolean genero) {
+        this.genero = genero;
+    }
+
+    public String getComplexion() {
+        return complexion;
+    }
+
+    public void setComplexion(String complexion) {
+        this.complexion = complexion;
+    }
+
+    public String getUrlImagen() {
+        return urlImagen;
+    }
+
+    public void setUrlImagen(String urlImagen) {
+        this.urlImagen = urlImagen;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Especie getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(Especie especie) {
+        this.especie = especie;
+    }
 }

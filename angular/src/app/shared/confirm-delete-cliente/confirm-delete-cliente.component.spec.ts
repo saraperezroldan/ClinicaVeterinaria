f078544ad7fe7f41cab3926f520d90cb967d4c38
lucia <luucia.lopezc@gmail.com>
2024-10-12 01:58:03 +0200

    Implementacion inicio-usuario y dialog para eliminar una mascota

--- angular/src/app/shared/confirm-delete-cliente/confirm-delete-cliente.component.spec.ts
+++ angular/src/app/shared/confirm-delete-cliente/confirm-delete-cliente.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ConfirmDeleteClienteComponent } from './confirm-delete-cliente.component';
+import { ConfirmDeleteClienteComponent } from '././confirm-delete-cliente.component';
 
 describe('ConfirmDeleteComponent', () => {
   let component: ConfirmDeleteClienteComponent;
